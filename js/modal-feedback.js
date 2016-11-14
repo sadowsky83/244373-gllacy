06fecb80a560ee29dab336634fe4920c6feefea8
Anatoly Sadowsky <sadowsky83@gmail.com>
2016-11-14 18:38:24 +0700

    исправление

--- js/modal-feedback.js
+++ js/modal-feedback.js
@@ -10,35 +10,35 @@ var storage1 = localStorage.getItem("login");
 var storage2 = localStorage.getItem("email");
 
 link.addEventListener("click", function(event) {
-	event.preventDefault();
-	popup.classList.add("modal-content-show");
-	overlay.classList.add("overlay-show");
-	login.focus();
+  event.preventDefault();
+  popup.classList.add("modal-content-show");
+  overlay.classList.add("overlay-show");
+  login.focus();
 
-	if (storage1) {
-		login.value = storage1;
-		if (storage2) {
-			email.value = storage2;
-			feedback.focus();
-		} else {
-			email.focus();
-		}
-	} else {
-		login.focus();
-	}
+  if (storage1) {
+    login.value = storage1;
+    if (storage2) {
+      email.value = storage2;
+      feedback.focus();
+    } else {
+      email.focus();
+    }
+  } else {
+    login.focus();
+  }
 });
 
 close.addEventListener("click", function(event) {
-	event.preventDefault();
-	popup.classList.remove("modal-content-show");
-	overlay.classList.remove("overlay-show");
+  event.preventDefault();
+  popup.classList.remove("modal-content-show");
+  overlay.classList.remove("overlay-show");
 });
 
-window,addEventListener("keydown", function(event) {
-	if (event.keyCode === 27) {
-		if (popup.classList.contains("modal-content-show")) {
-			popup.classList.remove("modal-content-show");
-			overlay.classList.remove("overlay-show");
-		}
-	}
+window, addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (popup.classList.contains("modal-content-show")) {
+      popup.classList.remove("modal-content-show");
+      overlay.classList.remove("overlay-show");
+    }
+  }
 });
